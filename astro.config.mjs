b991ff5b93c784de3cd93b0f1a281b8c9fb0f634
Axii <axii@sju.ac.kr>
2026-04-05 22:03:10 +0900

    Update site URL to match repo name

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://0xaxii.github.io",
+	site: "https://0xAxii.github.io",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
